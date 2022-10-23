26045b8c5f0fe16ad5e580a442eeaa77592cbc30
pkr28 <pkr2722@yahoo.com>
2022-10-23 13:09:48 -0400

    Updated frontend

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -19,32 +19,39 @@ if [ $USERID -ne 0 ] ; then
     exit 1
 fi
 
-echo -n " Installing Nginx : " &>> $LOG
-yum install nginx -y 
+echo -n " Installing Nginx : " 
+yum install nginx -y &>> $LOG
 stat $?
 
 # echo "Staring nginx service "
 # systemctl enable nginx
 # systemctl start nginx
+# stat $?
 
 # echo "Downloading the $COMPONENT :"
 # curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+# stat $?
 
 # echo "Moving to $COMPONENT :"
 # cd /usr/share/nginx/html
 # rm -rf *
+# stat $?
 
 # echo "unzip $COMPONENT files: "
 # unzip /tmp/$COMPONENT.zip
+# stat $?
 
 # echo "Moving $COMPONENT files:"
 # mv $COMPONENT-main/* .
 # mv static/* .
 # rm -rf $COMPONENT-main README.md
+# stat $?
 
 # echo "Configuring proxy file:"
 # mv localhost.conf /etc/nginx/default.d/roboshop.conf
+# stat $?
 
 # echo "Restaring the service: "
 # systemctl restart nginx
 # systemctl status nginx -l
+# stat $?
