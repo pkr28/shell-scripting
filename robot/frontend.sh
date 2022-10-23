446ad0e87b383cb05f30deb3b0da5bde713b7302
pkr28 <pkr2722@yahoo.com>
2022-10-23 11:57:56 -0400

    Frontend

---
+++ robot/frontend.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+COMPONENT=$COMPONENT
+
+if [ $USERID eq 0 ] then
+    echo "Running as root User" ;
+fi
+
+echo " Installing Nginx : "
+yum install nginx -y
+
+# echo "Staring nginx service "
+# systemctl enable nginx
+# systemctl start nginx
+
+# echo "Downloading the $COMPONENT :"
+# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+
+# echo "Moving to $COMPONENT :"
+# cd /usr/share/nginx/html
+# rm -rf *
+
+# echo "unzip $COMPONENT files: "
+# unzip /tmp/$COMPONENT.zip
+
+# echo "Moving $COMPONENT files:"
+# mv $COMPONENT-main/* .
+# mv static/* .
+# rm -rf $COMPONENT-main README.md
+
+# echo "Configuring proxy file:"
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+# echo "Restaring the service: "
+# systemctl restart nginx
+# systemctl status nginx
+
