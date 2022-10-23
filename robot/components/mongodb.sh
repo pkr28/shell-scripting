f96071b7b8cfefd2096172077f62b271aefc379a
pkr28 <pkr2722@yahoo.com>
2022-10-23 14:31:38 -0400

    Updated frontend

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -5,34 +5,34 @@ COMPONENT=mongodb
 source components/common.sh
 
 echo -n "Downloading repo :"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo &>> $LOG 
+curl -s -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/mongo.repo &>> $LOG 
 stat $?
 
-echo -n "Installing mongodb : "
-yum install -y mongodb-org &>> $LOG
+echo -n "Installing $COMPONENT : "
+yum install -y $COMPONENT-org &>> $LOG
 stat $?
 
 echo -n "Editing the conf file :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
 stat $?
 
-echo -n "Starting mongodb service"
+echo -n "Starting $COMPONENT service"
 systemctl enable mongod
 systemctl start mongod
 stat $?
 
-# echo -n "Downloading mongodb schema :"
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip" &>> $LOG
+# echo -n "Downloading $COMPONENT schema :"
+# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOG
 # stat $?
 
-# echo -n "Unzipping mongodb schema"
+# echo -n "Unzipping $COMPONENT schema"
 # cd /tmp
-# unzip mongodb.zip &>> $LOG
-# cd mongodb-main
+# unzip $COMPONENT.zip &>> $LOG
+# cd $COMPONENT-main
 # mongo < catalogue.js &>> $LOG
 # mongo < users.js &>> $LOG
 # stat $?
 
-# echo -n "Restarting mongodb service"
+# echo -n "Restarting $COMPONENT service"
 # systemctl restart mongod
 # stat $?
