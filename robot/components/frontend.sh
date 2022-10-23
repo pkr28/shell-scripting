236372fd42e0e8de9436e36087d4dffe8933208e
pkr28 <pkr2722@yahoo.com>
2022-10-23 13:13:56 -0400

    Updated frontend

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -23,35 +23,35 @@ echo -n " Installing Nginx : "
 yum install nginx -y &>> /tmp/frontend.log
 stat $?
 
-# echo "Staring nginx service "
-# systemctl enable nginx
-# systemctl start nginx
-# stat $?
+echo "Staring nginx service "
+systemctl enable nginx
+systemctl start nginx
+stat $?
 
-# echo "Downloading the $COMPONENT :"
-# curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-# stat $?
+echo "Downloading the $COMPONENT :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
 
-# echo "Moving to $COMPONENT :"
-# cd /usr/share/nginx/html
-# rm -rf *
-# stat $?
+echo "Moving to $COMPONENT :"
+cd /usr/share/nginx/html
+rm -rf *
+stat $?
 
-# echo "unzip $COMPONENT files: "
-# unzip /tmp/$COMPONENT.zip
-# stat $?
+echo "unzip $COMPONENT files: "
+unzip /tmp/$COMPONENT.zip
+stat $?
 
-# echo "Moving $COMPONENT files:"
-# mv $COMPONENT-main/* .
-# mv static/* .
-# rm -rf $COMPONENT-main README.md
-# stat $?
+echo "Moving $COMPONENT files:"
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md
+stat $?
 
-# echo "Configuring proxy file:"
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-# stat $?
+echo "Configuring proxy file:"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
-# echo "Restaring the service: "
-# systemctl restart nginx
-# systemctl status nginx -l
-# stat $?
+echo "Restaring the service: "
+systemctl restart nginx
+systemctl status nginx -l
+stat $?
